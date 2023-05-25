0dee230c0b32828b64649b9ed3901b9347f670a5
Sam <samceen@gmail.com>
2023-05-25 15:49:46 -0400

    add changes

--- aws-ses/main.tf
+++ aws-ses/main.tf
@@ -4,10 +4,18 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
